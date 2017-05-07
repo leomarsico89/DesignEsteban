df7fe9c2b5977bb4543ef5f6f03e4cc935f8ab25
Walter Alejandro Camaly <waltercamaly@gmail.com>
2017-05-07 19:51:44 -0300

    wally

--- public/js/arriba.js
+++ public/js/arriba.js
@@ -1,26 +1,25 @@
-$(document).ready(function(){
+(function($, w, d) {
 
-   $('.ir-arriba').click(function(){
-    $('body, html').animate({
+  function startApp() {
+      // EVENT BOTTON  GO TOP 
+      $('.ir-arriba').click(function() {
+          $('body, html').animate({
+              scrollTop: '0px'
+          }, 300);
+      });
+      // ANIMATION SCROLL
+      $(w).scroll(function() {
+          
+          if ($(this).scrollTop() > 0) {
+              $('.ir-arriba').slideDown(300);// SHOW BOTTON GO TOP
+              $('.nav_header').addClass('scroll');// ADD BACKGROUND HEADER
+          } else {
+              $('.ir-arriba').slideUp(300);// HIDE BOTTON GO TOP
+              $('.nav_header').removeClass('scroll');// REMOVE BACKGROUND HEADER
+          }
+      });
 
-    	scrollTop:'0px'
+  }
+  $(d).ready(startApp);
 
-    }, 300);
-
-   });
-
-   $(window).scroll(function(){
-
-   if( $(this).scrollTop() > 0 ){
-
-   	  $('.ir-arriba').slideDown(300);
-
-   	} else {
-
-         $('.ir-arriba').slideUp(300);
-
-   	}
-
-   });
-
-});
+})(jQuery, window, document)
